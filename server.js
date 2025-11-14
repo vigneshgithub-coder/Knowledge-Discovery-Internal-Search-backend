fb690d9f27f1619b750b69198ba9cbdb00ff077c
vigneshgithub-coder <vigneshwaranmca22@gmail.com>
2025-11-14 23:24:50 +0530

    Update allowedOrigins for CORS configuration

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const port = process.env.PORT || 5000;
 // CORS configuration for production
 const allowedOrigins = process.env.ALLOWED_ORIGINS 
   ? process.env.ALLOWED_ORIGINS.split(',')
-  : ['http://localhost:5173', 'http://localhost:3000'];
+  : ['http://localhost:5173', 'https://knowledgediscoveryinternalsearch.vercel.app/search'];
 
 app.use(cors({
   origin: function (origin, callback) {
